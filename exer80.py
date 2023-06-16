15d7774e4de4f84710d52ec11d47753e1081d58a
Triliam <taisbuenovidotto@gmail.com>
2023-06-16 09:29:44 -0300

    exer77

--- exer80.py
+++ exer80.py
@@ -1,6 +1,7 @@
 fatorial = 1
 
-for i in range(1,5): #fatorial de 4
+for i in range(1,6): #fatorial de 4
     fatorial = fatorial * i
+    print(fatorial, " = ", fatorial, " x ", i)
 
 print(fatorial)
